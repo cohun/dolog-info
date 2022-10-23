da1b5891c76b6df36f35e80263e50b93e0802a81
Colin Hunwald <colin.hunwald@gmail.com>
2022-10-23 20:20:18 +0200

    Add GoogleAuthProvider

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,6 +3,7 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 import { getStorage } from 'firebase/storage';
+import { GoogleAuthProvider } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,5 +18,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
 export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
 export const storage = getStorage(app);
