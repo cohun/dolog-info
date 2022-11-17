1e1a24300d7b18c9f0ed37ba46d9c3756252231c
Colin Hunwald <colin.hunwald@gmail.com>
2022-11-17 17:44:45 +0100

    UserProfile companies button

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -1,16 +1,42 @@
-import { query, where } from "firebase/firestore";
+import { query, where } from 'firebase/firestore';
 
 const UserProfile = ({ username, companies }) => {
   console.log(companies);
   return (
     <div>
-      <div className="container"></div>
-      <div className="container"></div>
-      <div className="container"></div>
       <div className="section">
         <div className="card">
-          <div>UserProfile {username}</div>
-          <div className="box">I am in a box. {companies[1]}</div>
+          <h3 className="title">
+            {username} az alábbi tulajdonosok dolgaihoz fér hozzá:
+          </h3>
+        </div>
+      </div>
+      <div className="section">
+        <div className="">
+          <div className="columns is-centered is-8">
+            <div className=" button is-large is-info is-outlined">
+              {companies[1]}
+            </div>
+
+            <div className="box"></div>
+
+            <div className="button is-large is-focused is-info is-outlined">
+              Second column
+            </div>
+
+            <div className="box"></div>
+
+            <div className="button is-large is-info is-outlined">
+              Third column
+            </div>
+
+            <div className="box"></div>
+
+            <div className="button is-large is-info is-outlined">
+              Fourth column
+            </div>
+          </div>
+          <div className="">I am in a box. {companies[1]}</div>
         </div>
       </div>
     </div>
