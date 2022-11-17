d84e1ca1cc74fcb92e3d9bf3f39769d9121a52d2
Colin Hunwald <colin.hunwald@gmail.com>
2022-11-17 19:00:08 +0100

    UserProfile companies button 2

--- components/UserProfile.js
+++ components/UserProfile.js
@@ -4,39 +4,26 @@ const UserProfile = ({ username, companies }) => {
   console.log(companies);
   return (
     <div>
-      <div className="section">
-        <div className="card">
-          <h3 className="title">
-            {username} az alábbi tulajdonosok dolgaihoz fér hozzá:
+      <div className="section is-info">
+        <div className="columns is-centered ">
+          <h3 className="card column is-half title has-text-centered has-background-primary">
+            <strong className="is-capitalized">{username} </strong>
+            az alábbi tulajdonosok dolgaihoz fér hozzá:
           </h3>
         </div>
       </div>
       <div className="section">
         <div className="">
           <div className="columns is-centered is-8">
-            <div className=" button is-large is-info is-outlined">
-              {companies[1]}
-            </div>
-
-            <div className="box"></div>
-
-            <div className="button is-large is-focused is-info is-outlined">
-              Second column
-            </div>
-
-            <div className="box"></div>
-
-            <div className="button is-large is-info is-outlined">
-              Third column
-            </div>
-
-            <div className="box"></div>
-
-            <div className="button is-large is-info is-outlined">
-              Fourth column
-            </div>
+            {companies.map((company) => (
+              <div
+                key={company.id}
+                className=" button is-large is-info is-outlined mr-6"
+              >
+                {company}
+              </div>
+            ))}
           </div>
-          <div className="">I am in a box. {companies[1]}</div>
         </div>
       </div>
     </div>
