2525a3fb75a477ec085397673a58a7f1c1b1c787
Colin Hunwald <colin.hunwald@gmail.com>
2023-01-02 18:26:06 +0100

    admin PostList filtered according company

--- components/PostList.js
+++ components/PostList.js
@@ -1,41 +1,40 @@
-import toast from "react-hot-toast";
-import AuthCheck from "./AuthCheck";
-import kebabCase from "lodash.kebabcase";
-import { auth, db } from "../lib/firebaseConfig";
-import { collection, query, where, getDocs, orderBy } from "firebase/firestore";
-import { postToJson, fromMillis } from "../lib/firebaseConfig";
-import PostFeed from "./PostFeed";
-import { useEffect, useState } from "react";
+import toast from 'react-hot-toast';
+import AuthCheck from './AuthCheck';
+import kebabCase from 'lodash.kebabcase';
+import { auth, db } from '../lib/firebaseConfig';
+import { collection, query, where, getDocs, orderBy } from 'firebase/firestore';
+import { postToJson, fromMillis } from '../lib/firebaseConfig';
+import PostFeed from './PostFeed';
+import { useEffect, useState } from 'react';
 
-const PostList = () => {
+const PostList = ({ username, company }) => {
   const uid = auth.currentUser.uid;
   const [post, setPost] = useState([]);
   async function GetList(uid) {
-    console.log("UID:", uid);
-
     const querySnapshot = await getDocs(
       collection(db, `users/${uid}/posts`),
-      orderBy("createdAt")
+      orderBy('createdAt')
     );
     querySnapshot.forEach((doc) => {
       // doc.data() is never undefined for query doc snapshots
-      console.log(doc.id, " => ", doc.data());
     });
     const posts = querySnapshot?.docs.map(postToJson);
-    setPost(posts);
+
+    if (posts) {
+      const filteredPosts = posts.filter((post) => post.company === company);
+      setPost(filteredPosts);
+    }
   }
   useEffect(() => {
     GetList(uid);
-  }, []);
-
-  console.log(`users/${uid}/posts`);
+  }, [company]);
 
   return (
     <>
       <h1 className="title has-text-centered has-text-grey-light is-underlined">
         Szerkeszd posztjaidat
       </h1>
-      <PostFeed posts={post} admin />
+      <PostFeed posts={post} username={username} company={company} />
     </>
   );
 };
--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
-import toast from "react-hot-toast";
-import AuthCheck from "../../components/AuthCheck";
-import kebabCase from "lodash.kebabcase";
-import { useContext } from "react";
-import { UserContext } from "../../lib/context";
+import React, { useEffect, useState } from 'react';
+import toast from 'react-hot-toast';
+import AuthCheck from '../../components/AuthCheck';
+import kebabCase from 'lodash.kebabcase';
+import { useContext } from 'react';
+import { UserContext } from '../../lib/context';
 import {
   collection,
   query,
@@ -11,17 +11,52 @@ import {
   getDocs,
   orderBy,
   db,
-} from "firebase/firestore";
-import PostFeed from "../../components/PostFeed";
-import { postToJson, fromMillis } from "../../lib/firebaseConfig";
-import PostList from "../../components/PostList";
-import Navbar from "../../components/Navbar";
-import { useRouter } from "next/router";
-import Image from "next/image";
+} from 'firebase/firestore';
+import PostFeed from '../../components/PostFeed';
+import {
+  postToJson,
+  fromMillis,
+  getAllCompaniesForAUser,
+} from '../../lib/firebaseConfig';
+import PostList from '../../components/PostList';
+import Navbar from '../../components/Navbar';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 const AdminPostsPage = () => {
   const { user, username } = useContext(UserContext);
-  const [isActive, setIsActive] = useState("");
+  const [isActive, setIsActive] = useState('');
+  const [companies, setCompanies] = useState([]);
+  const [chosen, setChosen] = useState('');
+
+  async function getAllCompanies() {
+    const comp = await getAllCompaniesForAUser(username);
+    setCompanies(comp);
+  }
+
+  useEffect(() => {
+    getAllCompanies();
+    DropDown();
+  }, [isActive]);
+
+  function DropDown() {
+    const list = [];
+    companies.forEach((comp) => {
+      list.push(
+        <div>
+          <a
+            href="#"
+            className="dropdown-item"
+            onClick={() => setChosen(comp.businessName)}
+          >
+            {comp.businessName} - {comp.address}
+          </a>
+          <hr className="dropdown-divider" />
+        </div>
+      );
+    });
+    return list;
+  }
 
   return (
     <main>
@@ -30,39 +65,29 @@ const AdminPostsPage = () => {
         <div className="section has-background-black-ter">
           <div
             className={`dropdown ${isActive}`}
-            onClick={() => {
-              isActive === "" ? setIsActive("is-active") : setIsActive("");
+            onClick={(e) => {
+              e.preventDefault();
+              isActive === '' ? setIsActive('is-active') : setIsActive('');
             }}
           >
             <div className="dropdown-trigger">
               <button
-                className="button"
+                className={
+                  chosen === '' ? 'button' : 'button is-primary is-outlined'
+                }
                 aria-haspopup="true"
                 aria-controls="dropdown-menu"
               >
-                <span>Cég kiválasztása</span>
+                <span className="has-text--primary">
+                  {chosen === '' ? 'Cég kiválasztása' : chosen}
+                </span>
                 <span className="icon is-small">
                   <i className="fas fa-angle-down" aria-hidden="true"></i>
                 </span>
               </button>
             </div>
             <div className="dropdown-menu" id="dropdown-menu" role="menu">
-              <div className="dropdown-content">
-                <a href="#" className="dropdown-item">
-                  Dropdown item
-                </a>
-                <a className="dropdown-item">Other dropdown item</a>
-                <a href="#" className="dropdown-item is-active">
-                  Active dropdown item
-                </a>
-                <a href="#" className="dropdown-item">
-                  Other dropdown item
-                </a>
-                <hr className="dropdown-divider" />
-                <a href="#" className="dropdown-item">
-                  With a divider
-                </a>
-              </div>
+              <div className="dropdown-content">{DropDown()}</div>
             </div>
           </div>
           <h1 className="title has-text-centered has-text-grey-light is-underlined">
@@ -75,7 +100,7 @@ const AdminPostsPage = () => {
                   src={
                     user?.photoURL
                       ? user.photoURL
-                      : "https://bulma.io/images/placeholders/128x128.png"
+                      : 'https://bulma.io/images/placeholders/128x128.png'
                   }
                   alt="4"
                   width={48}
@@ -86,7 +111,7 @@ const AdminPostsPage = () => {
             <div className="media-content">
               <div className="content">
                 <strong className="has-text-primary is-capitalized">
-                  {username}{" "}
+                  {username}{' '}
                 </strong>
               </div>
 
@@ -101,14 +126,19 @@ const AdminPostsPage = () => {
               </div>
               <div className="field">
                 <div className="control">
-                  <a className="button is-primary">
+                  <a
+                    className="button is-primary"
+                    onClick={() => {
+                      chosen === '' ? toast.error('Válassz egy céget') : null;
+                    }}
+                  >
                     <strong>Poszt írás</strong>
                   </a>
                 </div>
               </div>
             </div>
           </article>
-          <PostList />
+          <PostList username={username} company={chosen} />
           <hr />
           <CreateNewPost />
         </div>
@@ -122,7 +152,7 @@ const AdminPostsPage = () => {
 function CreateNewPost() {
   const router = useRouter();
   const { username } = useContext(UserContext);
-  const [title, setTitle] = useState("");
+  const [title, setTitle] = useState('');
   const slug = encodeURI(kebabCase(title));
   const isValid = title.length > 3 && title.length < 100;
 }
