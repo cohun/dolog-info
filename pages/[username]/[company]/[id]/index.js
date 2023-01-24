6c0a8d01dfd50fe24f0b3f8c384bf8691a352f92
Colin Hunwald <colin.hunwald@gmail.com>
2023-01-24 18:04:35 +0100

    delPosts modal

--- pages/[username]/[company]/[id]/index.js
+++ pages/[username]/[company]/[id]/index.js
@@ -1,6 +1,6 @@
-import Link from "next/link";
-import Image from "next/image";
-import { useRouter } from "next/router";
+import Link from 'next/link';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import {
   collectionGroup,
   query,
@@ -13,25 +13,25 @@ import {
   startAfter,
   collection,
   deleteDoc,
-} from "firebase/firestore";
-import { useEffect, useState } from "react";
-import Navbar from "../../../../components/Navbar";
-import { useContext } from "react";
-import { UserContext } from "../../../../lib/context";
-import PostFeed from "../../../../components/PostFeed";
+} from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import Navbar from '../../../../components/Navbar';
+import { useContext } from 'react';
+import { UserContext } from '../../../../lib/context';
+import PostFeed from '../../../../components/PostFeed';
 import {
   postToJson,
   fromMillis,
   db,
   delThing,
-} from "../../../../lib/firebaseConfig";
-import AuthCheck from "../../../../components/AuthCheck";
+} from '../../../../lib/firebaseConfig';
+import AuthCheck from '../../../../components/AuthCheck';
 
 const LIMIT = 5;
 
 const ComingFromQRCode = () => {
   const [imageURL, setImageURL] = useState(
-    "https://bulma.io/images/placeholders/128x128.png"
+    'https://bulma.io/images/placeholders/128x128.png'
   );
   const [loading, setLoading] = useState(false);
   const [postsEnd, setPostsEnd] = useState(false);
@@ -41,6 +41,7 @@ const ComingFromQRCode = () => {
   const [filteredPosts, setFilteredPosts] = useState([]);
   const [users, setUsers] = useState([]);
   const [things, setThings] = useState([]);
+  const [del, setDel] = useState(false);
   const { user, username } = useContext(UserContext);
   const router = useRouter();
   const { username: un, company, id } = router.query;
@@ -64,7 +65,7 @@ const ComingFromQRCode = () => {
       thingsIdincluded(company, username);
     }
     if (things.includes(id)) {
-      console.log("here");
+      console.log('here');
       setIsAdmitted(true);
     } else {
       if (isAdmin) {
@@ -76,20 +77,20 @@ const ComingFromQRCode = () => {
   }, [isAdmitted, things.length, username, company, isAdmitted2]);
 
   const thingsIdincluded = async function (target, who) {
-    const docRef = doc(db, `companies/${target}/${who}`, "what");
+    const docRef = doc(db, `companies/${target}/${who}`, 'what');
 
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log("things", docSnap.data().thingsId, id);
+      console.log('things', docSnap.data().thingsId, id);
       setThings(docSnap.data().thingsId);
-      console.log("th", things, "+", id);
+      console.log('th', things, '+', id);
 
-      console.log("isAdmitted2:", isAdmitted);
+      console.log('isAdmitted2:', isAdmitted);
       setIsAdmitted2(isAdmitted);
     } else {
       // doc.data() will be undefined in this case
-      console.log("No such document!");
+      console.log('No such document!');
     }
   };
 
@@ -109,11 +110,11 @@ const ComingFromQRCode = () => {
       }
       console.log(who);
       console.log(users);
-      console.log("un.", un);
-      console.log("isAdmitted:", isAdmitted);
+      console.log('un.', un);
+      console.log('isAdmitted:', isAdmitted);
     } else {
       // doc.data() will be undefined in this case
-      console.log("No such document!");
+      console.log('No such document!');
     }
   };
 
@@ -129,34 +130,34 @@ const ComingFromQRCode = () => {
   }
 
   const getUID = async function (user) {
-    const docRef = doc(db, "usernames", user);
+    const docRef = doc(db, 'usernames', user);
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log("Document data:", docSnap.data());
+      console.log('Document data:', docSnap.data());
       const uid = docSnap.data().uid;
       const querySnapshot = await getDocs(
         collection(db, `users/${uid}/${company}/${id}/posts`)
       );
       querySnapshot.forEach((doc) => {
         // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
+        console.log(doc.id, ' => ', doc.data());
         delPost(uid, doc.id);
       });
-      router.push("/");
+      router.push('/');
     } else {
       // doc.data() will be undefined in this case
-      console.log("No such document!");
+      console.log('No such document!');
     }
   };
 
   const getPosts = async function (company) {
     const postsQuery = query(
-      collectionGroup(db, "posts"),
-      where("published", "==", true),
-      where("company", "==", company),
-      where("id", "==", id),
-      orderBy("createdAt", "desc"),
+      collectionGroup(db, 'posts'),
+      where('published', '==', true),
+      where('company', '==', company),
+      where('id', '==', id),
+      orderBy('createdAt', 'desc'),
       limit(LIMIT)
     );
     const posts = (await getDocs(postsQuery)).docs.map(postToJson);
@@ -167,15 +168,15 @@ const ComingFromQRCode = () => {
     setLoading(true);
     const last = filteredPosts[filteredPosts.length - 1];
     const cursor =
-      typeof last.createdAt === "number"
+      typeof last.createdAt === 'number'
         ? fromMillis(last.createdAt)
         : last.createdAt;
     const postsQuery = query(
-      collectionGroup(db, "posts"),
-      where("published", "==", true),
-      where("company", "==", company),
-      where("id", "==", id),
-      orderBy("createdAt", "desc"),
+      collectionGroup(db, 'posts'),
+      where('published', '==', true),
+      where('company', '==', company),
+      where('id', '==', id),
+      orderBy('createdAt', 'desc'),
       startAfter(cursor),
       limit(LIMIT)
     );
@@ -234,6 +235,43 @@ const ComingFromQRCode = () => {
 
                     {isAdmin && (
                       <div className="level-item has-text-centered">
+                        {del ? (
+                          <div className="card has-background-link">
+                            <div className="modal is-active">
+                              <div className="modal-background"></div>
+                              <div className="modal-card">
+                                <header className="modal-card-head">
+                                  <p className="modal-card-title has-text-danger">
+                                    A dolog és a hozzátartozó valamennyi poszt
+                                    törlése
+                                  </p>
+                                  <button
+                                    className="delete"
+                                    aria-label="close"
+                                    onClick={() => setDel(false)}
+                                  ></button>
+                                </header>
+                                <section className="modal-card-body">
+                                  Biztos vagy benne?
+                                </section>
+                                <footer className="modal-card-foot">
+                                  <button
+                                    onClick={() => delPosts()}
+                                    className="button is-success"
+                                  >
+                                    Igen, töröld
+                                  </button>
+                                  <button
+                                    onClick={() => setDel(false)}
+                                    className="button"
+                                  >
+                                    Vissza
+                                  </button>
+                                </footer>
+                              </div>
+                            </div>
+                          </div>
+                        ) : null}
                         <div>
                           <p className="heading">
                             dolog és valamennyi hozzátartozó poszt törlése
@@ -242,7 +280,7 @@ const ComingFromQRCode = () => {
                             <button
                               className="button is-danger is-light"
                               onClick={(event) => {
-                                delPosts();
+                                setDel(!del);
                               }}
                             >
                               Törlés
@@ -292,7 +330,7 @@ const ComingFromQRCode = () => {
                     <div className="content">
                       <div className="">
                         <strong className="has-text-primary is-capitalized mr-2">
-                          {username}{" "}
+                          {username}{' '}
                         </strong>
                         <span className="">
                           <Link href="/admin">
